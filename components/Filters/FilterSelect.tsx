91afbe700c677f1e95e9cc255cdd806cd9fa0ef9
kyle <kylegutierrez51@gmail.com>
2026-06-03 00:47:18 -0400

    Update FilterSelect component so that a page can optionally use its default media queries

--- components/Filters/FilterSelect.tsx
+++ components/Filters/FilterSelect.tsx
@@ -5,14 +5,15 @@ interface FilterSelectProps {
   id: string;
   name: string;
   options: { value: string; label: string }[];
-  defaultValue?: string
-  styles?: Record<string, string>
+  defaultValue?: string;
+  styles?: Record<string, string>; // Approvals, Webhooks, Run Detail //
+  responsive?: boolean // Pipeline Editor //
 }
 
 
-export default function FilterSelect({ id, name, options, defaultValue, styles=defaultStyles }: FilterSelectProps) {
+export default function FilterSelect({ id, name, options, defaultValue, styles=defaultStyles, responsive=true }: FilterSelectProps) {
   return (
-    <div className={styles['select-group']}>
+    <div className={`${styles['select-group']}${responsive ? ` ${styles.responsive}` : ''}`}>
       <select id={id} name={name} defaultValue={defaultValue}>
         {options.map((option, index) => (
           <option value={option.value} key={index}>{option.label}</option>
