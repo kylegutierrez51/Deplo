78006fcd8b14f5d6fd54157d9baa82bb2578ab5b
kyle <kylegutierrez51@gmail.com>
2026-06-05 01:54:04 -0400

    feat: add EnvVarsSection subcomponent

---
+++ components/PipelineEditor/StageSidebar/EnvVarsSection.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import styles from './stage-sidebar.module.css'
+import EnvVarRow from './EnvVarRow'
+
+interface EnvVarsSectionProps {
+  vars: { key: string; value: string }[];
+  onAdd: () => void;
+  onChange: (index: number, field: 'key' | 'value', value: string) => void;
+}
+
+export default function EnvVarsSection({ vars, onAdd, onChange }: EnvVarsSectionProps) {
+  return (
+    <div className={styles['env-vars']}>
+      <div className={styles['env-title-container']}>
+        <div className={styles['env-title']}>
+          <ion-icon name="settings-outline"></ion-icon>
+          <label>ENV VARIABLES</label>
+        </div>
+        <button className={styles['add-env-btn']} type="button" onClick={onAdd}>
+          <ion-icon name="add-outline"></ion-icon>
+          Add
+        </button>
+      </div>
+      <div className={styles['env-vars-list']}>
+        {vars.map((v, i) => (
+          <EnvVarRow
+            key={i}
+            envKey={v.key}
+            envValue={v.value}
+            onChange={(field, value) => onChange(i, field, value)}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
