76690ac29e32bdade7b671140b7170639b27edea
kyle <kylegutierrez51@gmail.com>
2026-06-13 22:23:58 -0400

    Allow a github image to be avilable in the session for use in profile in sidebar

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  
+  images: {
+    domains: ["avatars.githubusercontent.com"] // allows a github image to be available in the session (session.user.image)
+  }
 };
 
 export default nextConfig;
