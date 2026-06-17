325b3a02410c45ec871272188f14b44ecf81e476
kyle <kylegutierrez51@gmail.com>
2026-06-17 18:19:50 -0400

    Move Subheader component to its own folder (components/subheader). Create AddButton, RefreshButton, and ExportButton to cover all subheader buttons used throughout page

--- components/subheader/AddButton.tsx
+++ components/subheader/AddButton.tsx
@@ -2,13 +2,13 @@
 
 import { useRouter } from "next/navigation";
 
-export default function AddSecretButton() {
+export default function AddButton({ text, url }: { text: string, url: string }) {
   const router = useRouter();
 
   return (
-    <button onClick={() => router.push("/secrets?mode=create")}>
+    <button onClick={() => router.push(`/${url}?mode=create`)}>
       <ion-icon name="add-outline"></ion-icon>
-      Add Secret
+      {text}
     </button>
   )
 }
---
+++ components/subheader/ExportButton.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function ExportButton() {
+  return (
+    <button>
+      <ion-icon name="download-outline"></ion-icon>
+      Export
+    </button>
+  )
+}
