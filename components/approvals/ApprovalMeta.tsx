56b5a35ea80d8f88a4db755f419779dc83463b5a
kyle <kylegutierrez51@gmail.com>
2026-06-21 22:01:41 -0400

    Update approvals page to loop through ApprovalCard components with sample data from @/lib/data/approvals. Since that file has a different type structure, update affected components' props so that they align with the structure of the sample approvals array in page.tsx.

--- components/approvals/ApprovalMeta.tsx
+++ components/approvals/ApprovalMeta.tsx
@@ -1,17 +1,17 @@
 import styles from './approval-meta.module.css'
 
 interface ApprovalMetaProps {
-  author: string;
+  createdBy: string | null;
   branch: string;
   waitingTime: string;
   stagesComplete: string;
 }
-export default function ApprovalMeta({ author, branch, waitingTime, stagesComplete }: ApprovalMetaProps) {
+export default function ApprovalMeta({ createdBy, branch, waitingTime, stagesComplete }: ApprovalMetaProps) {
   return (
     <div className={styles['extra-info']}>
       <div className={styles['meta-row']}>
         <ion-icon name="person-outline"></ion-icon>
-        <span>{author}</span>
+        <span>{createdBy || 'Unknown Creator'}</span>
       </div>
       <div className={styles['meta-row']}>
         <ion-icon name="git-branch-outline"></ion-icon>
