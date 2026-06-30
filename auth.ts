25092a3de82c37970bf90655bc3249ffb688212a
kyle <kylegutierrez51@gmail.com>
2026-06-30 02:51:55 -0400

    Add jwt and session callbacks to persist user id across requests and expose it to session.user.id so that Prisma can properly display what user creates an environment

--- auth.ts
+++ auth.ts
@@ -8,5 +8,15 @@ import prisma from "@/lib/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  adapter: PrismaAdapter(prisma) // this automatically populates tables in your database with the info of the user who's authenticated (as long as you have the schema setup for next-auth)
+  adapter: PrismaAdapter(prisma), // this automatically populates tables in your database with the info of the user who's authenticated (as long as you have the schema setup for next-auth)
+  callbacks: {
+    jwt({ token, user }) {
+       if (user) token.sub = user.id;
+       return token;
+    },
+    session({ session, token }) {
+      if (token.sub) session.user.id = token.sub;
+      return session;
+    }
+  }
 });
