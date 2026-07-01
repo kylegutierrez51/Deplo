f58e93b0dc16ae4159fdb30474a47f341b39dda0
kyle <kylegutierrez51@gmail.com>
2026-06-30 23:23:13 -0400

    Make it so user must wait 3 seconds before the delete button in Delete Confirmation modal becomes clickable. Space out the buttons properly in the CSS and style the delete button when it's unclickable

--- components/modals/DeleteConfirmation.tsx
+++ components/modals/DeleteConfirmation.tsx
@@ -1,5 +1,7 @@
+"use client"
+
 import styles from './delete-confirmation.module.css';
-import { useTransition } from 'react';
+import { useTransition, useState, useEffect } from 'react';
 import { deleteEnvironment } from "@/lib/actions/environments";
 
 interface DeleteConfirmationProps {
@@ -9,6 +11,15 @@ interface DeleteConfirmationProps {
 }
 export default function DeleteConfirmation({ id, onDelete, onDeleteClose }: DeleteConfirmationProps) {
   const [_isDeleteTransitionPending, startDeleteTransition] = useTransition();
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setReady(true)
+    }, 3000)
+
+    return () => clearTimeout(timer);
+  }, [])
 
   const handleDelete = () => startDeleteTransition(async () => {
     await deleteEnvironment(id);
@@ -17,11 +28,14 @@ export default function DeleteConfirmation({ id, onDelete, onDeleteClose }: Dele
 
   return (
     <div className={styles["delete-overlay"]} style={{ backdropFilter: 'blur(5px)', WebkitBackdropFilter: 'blur(5px)' }} onClick={onDeleteClose}>
-      <div className={styles["delete-container"]}>
+      <div className={styles["delete-container"]} onClick={e => e.stopPropagation()}>
         <p>Delete this Environment?</p>
-        <div>
+        <div className={styles['btn-group']}>
           <button className={styles["cancel-btn"]} type="button" onClick={onDeleteClose}>Cancel</button>
-          <button className={styles["delete-btn"]} type="button" onClick={handleDelete}>Delete</button>
+          {!ready ? 
+            <button className={styles["delete-ghost-btn"]} type="button">Delete</button> :
+            <button className={styles["delete-btn"]} type="button" onClick={handleDelete}>Delete</button> 
+          }
         </div>
       </div>
     </div>
