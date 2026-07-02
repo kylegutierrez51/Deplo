d2b703a385c9fe8cff81bae890b87b83c5926066
kyle <kylegutierrez51@gmail.com>
2026-07-02 01:27:03 -0400

    Update the get methods in secrets to fetch from real prisma data. Remove the sample data.

--- lib/data/secrets.ts
+++ lib/data/secrets.ts
@@ -1,31 +1,26 @@
-export type EnvType = 'production' | 'staging' | 'development' | 'preview' | 'custom';
+import prisma from '@/lib/prisma';
+import type { Secret as PrismaSecret } from '@/generated/prisma';
 
-export type Secret = {
-  id: number;
-  secretKey: string;
-  value: string;
-  notes?: string;
-  environmentType: EnvType;
-  environmentName: string;
-  createdBy: string | null;
-  createdAt: string;
-  updatedAt: string; 
-};
+export type Secret = Omit<PrismaSecret, 'encryptedValue' | 'authTag' | 'iv'>;
 
-export type SecretListItem = Omit<Secret, 'value'>;
-
-const SECRETS: Secret[] = [
-  { id: 1, secretKey: 'DATABASE_URL', value: 'asidaifaegauidfgaybaw2', notes: 'Primary Postgres connection — pool size 20, read replica enabled', environmentName: 'prod', environmentType: 'production', createdBy: 'sarah.chen', createdAt: '6/9/26, 21:27:34', updatedAt: '6/9/26, 21:27:34' },
-  { id: 2, secretKey: 'DATABASE_URL', value: "asidaifaegauidfgaybaw2", environmentName: 'staging', environmentType: 'staging', createdBy: 'sarah.chen', createdAt: '6/9/26, 21:27:34', updatedAt: '6/9/26, 21:27:34' },
-  { id: 3, secretKey: 'GITHUB_TOKEN', value: 'asidaifaegauidfgaybaw2', notes: 'Fine-grained PAT scoped to acme org, expires 2025-01-01', environmentName: 'dev', environmentType: 'development', createdBy: 'marcus.coco', createdAt: '6/9/26, 21:27:34', updatedAt: '6/9/26, 21:27:34' },
-  { id: 4, secretKey: 'GITHUB_TOKEN', value: 'asidaifaegauidfgaybaw2', notes: 'Fine-grained PAT scoped to acme org, expires 2025-01-01', environmentName: 'prev', environmentType: 'preview', createdBy: 'marcus.coco', createdAt: '6/9/26, 21:27:34', updatedAt: '6/9/26, 21:27:34' },
-  { id: 5, secretKey: 'GITHUB_TOKEN', value: 'asidaifaegauidfgaybaw2',  notes: 'Fine-grained PAT scoped to acme org, expires 2025-01-01', environmentName: 'custom', environmentType: 'custom', createdBy: null, createdAt: '6/9/26, 21:27:34', updatedAt: '6/9/26, 21:27:34' },
-];
-
-export async function getSecrets(): Promise<SecretListItem[]> {
-  return SECRETS.map(({ value: _, ...rest }) => rest);
+export async function getSecrets(): Promise<Secret[]> {
+  return await prisma.secret.findMany({
+    orderBy: { createdAt: "desc" },
+    include: {
+      environment: { 
+        select: { type: true, name: true}
+      }
+    }
+  });
 }
 
-export async function getSecretById(id: number): Promise<Secret | undefined> {
-  return SECRETS.find(s => s.id === id);
+export async function getSecretById(id: string): Promise<Secret | null> {
+  return await prisma.secret.findUnique({
+    where: { id },
+    include: {
+      environment: { 
+        select: { type: true, name: true}
+      }
+    }
+  });
 }
