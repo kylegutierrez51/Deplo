747552a3ef194669be97d12793c066c412f777c6
kyle <kylegutierrez51@gmail.com>
2026-07-24 15:12:38 -0400

    In SecretCheckboxRow, remove unused 'env' prop and remove its corresponding CSS

--- components/pipeline-editor/StageSidebar/SecretCheckboxRow.tsx
+++ components/pipeline-editor/StageSidebar/SecretCheckboxRow.tsx
@@ -4,19 +4,17 @@ import styles from './stage-sidebar.module.css'
 
 interface SecretCheckboxRowProps {
   secretKey: string;
-  env: string;
   checked: boolean;
   onToggle: () => void;
 }
 
-export default function SecretCheckboxRow({ secretKey, env, checked, onToggle }: SecretCheckboxRowProps) {
+export default function SecretCheckboxRow({ secretKey, checked, onToggle }: SecretCheckboxRowProps) {
   return (
     <label className={styles['secret-container']}>
       <div className={styles.secret}>
         <input type="checkbox" checked={checked} onChange={onToggle} />
         <span className={styles['secret-key']}>{secretKey}</span>
       </div>
-      <span className={styles['secret-env']}>{env}</span>
     </label>
   )
 }
